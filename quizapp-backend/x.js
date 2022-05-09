6f7094c36b4d9729986a3be965fa525ad1481810
pallishree-saswat <saswataeroarya@gmail.com>
2022-05-09 10:30:31 +0530

    first commit

---
+++ quizapp-backend/x.js
@@ -0,0 +1,8 @@
+let s = "Hello world from Fission Labs";
+let len = s.length;
+let word = s.split("")[0]
+for(let i = word.length -1 ; i < word.length; i++){
+    console.log(word.split(""))
+}
+
+//"olleH dlrow morf noissiF sbaL"
